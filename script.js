1ef21fd6e0361d8609fa580d74804f070236c0e3
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-07-28 23:33:50 -0300

    Add new function

---
+++ script.js
@@ -0,0 +1,33 @@
+const gameScreen = document.getElementById("gameScreen");
+
+let secretNumber;
+
+function start() {
+  buttonStart.removeEventListener("click", start);
+
+  document.getElementById("welcomeScreen").style.display = "none";
+  gameScreen.style.display = "flex";
+
+  secretNumber = Math.floor(Math.random() * 100 + 1);
+  console.log(secretNumber);
+}
+
+function adivinar() {
+  const numberUser = parseInt(document.getElementById("numberUser").value, 10);
+  console.log(numberUser);
+
+  const containerResult = document.getElementById("resultMessage");
+
+  if (secretNumber === numberUser) {
+    containerResult.innerHTML = "Ganaste";
+    document.getElementById("containerSecretNumber").innerHTML = secretNumber;
+  } else if (secretNumber !== numberUser) {
+    containerResult.innerHTML = "Intenta de nuevo";
+  }
+}
+
+document.getElementById("buttonTry").addEventListener("click", adivinar);
+
+window.onload = function () {
+  document.getElementById("buttonStart").addEventListener("click", start);
+};
